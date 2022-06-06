66feb0cecae42a0c68972bfa457c87777bff0e89
zhewei chen <1241770441@qq.com>
2022-06-06 08:31:02 +0800

    6.6日周一版本1

---
+++ web/middleware/auth1.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect
+from django.utils.deprecation import MiddlewareMixin
+
+
+class login_checking(MiddlewareMixin):
+    """利用中间件来验证是否登入了"""
+
+    def process_request(self, request):
+        if request.path_info in ["/login/"]:
+            return None
+        info_dict = request.session.get("info")
+        print(info_dict)
+        if info_dict:
+            return None
+        return redirect('/login/')
